3ffd2ce7a93b4be74536e58bc11f635fbc4c07aa
Steph Noutsa <stephanenoutsa@yahoo.com>
2016-06-04 22:26:34 +0100

    Removes a redundant import statement

--- app/src/main/java/android/internest/com/internest/PlaceholderFragment.java
+++ app/src/main/java/android/internest/com/internest/PlaceholderFragment.java
@@ -5,7 +5,6 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 /**
  * Created by stephnoutsa on 6/1/16.

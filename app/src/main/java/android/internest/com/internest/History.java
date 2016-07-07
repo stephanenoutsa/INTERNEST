ee2aa9c8420ceffcbfea26ebd12ef29ddeaec2d7
Steph Noutsa <stephanenoutsa@yahoo.com>
2016-07-07 17:49:16 +0100

    Handles the clicking of list items in the History activity

--- app/src/main/java/android/internest/com/internest/History.java
+++ app/src/main/java/android/internest/com/internest/History.java
@@ -1,8 +1,11 @@
 package android.internest.com.internest;
 
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Typeface;
+import android.net.Uri;
 import android.os.Bundle;
 //import android.support.design.widget.FloatingActionButton;
 //import android.support.design.widget.Snackbar;
@@ -12,6 +15,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -62,6 +67,53 @@ public class History extends AppCompatActivity {
                 // Set the adapter to display the scanned items
                 listView = (ListView) findViewById(R.id.scannedList);
                 listView.setAdapter(listAdapter);
+
+                listView.setOnItemClickListener(
+                        new AdapterView.OnItemClickListener() {
+                            @Override
+                            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                                TextView scanType = (TextView) findViewById(R.id.scanType);
+                                TextView scanDetails = (TextView) findViewById(R.id.scanDetails);
+
+                                String sType = scanType.getText().toString();
+                                final String sDetails = scanDetails.getText().toString();
+
+                                Intent intent;
+
+                                if (sType.equals("text")) {
+                                    intent = new Intent(context, TextDisplay.class);
+                                    intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                                    intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                                    intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+                                    intent.putExtra("text", sDetails);
+                                    startActivity(intent);
+                                }
+                                else if (sType.equals("url")) {
+                                    new AlertDialog.Builder(context).setIcon(android.R.drawable.ic_dialog_info).
+                                            setTitle("Open?").
+                                            setMessage("Do you want to open this in your browser?").
+                                            setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                                                @Override
+                                                public void onClick(DialogInterface dialog, int which) {
+                                                    Uri uri = Uri.parse(sDetails);
+                                                    Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                                                    startActivity(intent);
+                                                }
+                                            }).setNegativeButton("No", new DialogInterface.OnClickListener() {
+                                        @Override
+                                        public void onClick(DialogInterface dialog, int which) {
+                                            Intent intent = new Intent(getApplicationContext(), URLDisplay.class);
+                                            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                                            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                                            intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+                                            intent.putExtra("url", sDetails);
+                                            startActivity(intent);
+                                        }
+                                    }).show();
+                                }
+                            }
+                        }
+                );
             }
         };
 

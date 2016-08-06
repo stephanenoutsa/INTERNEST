e4bb7059edf6051809c133f7f282346b2c6bc6e5
Steph Noutsa <stephanenoutsa@yahoo.com>
2016-08-06 02:53:45 +0100

    Creates a Trend model for the Trending Fragment

---
+++ app/src/main/java/android/internest/com/internest/Trend.java
@@ -0,0 +1,52 @@
+package android.internest.com.internest;
+
+/**
+ * Created by stephnoutsa on 8/6/16.
+ */
+public class Trend {
+
+    // Private variables
+    int _tid, tcount;
+    String sdetails;
+
+    // Empty constructor
+    public Trend() {
+
+    }
+
+    // Constructor
+    public Trend(int _tid, String sdetails) {
+        this._tid = _tid;
+        this.sdetails = sdetails;
+    }
+
+    // Constructor
+    public Trend(String sdetails) {
+        this.sdetails = sdetails;
+    }
+
+    // Getter and Setter methods
+    public int get_tid() {
+        return _tid;
+    }
+
+    public void set_tid(int _tid) {
+        this._tid = _tid;
+    }
+
+    public String getSdetails() {
+        return sdetails;
+    }
+
+    public void setSdetails(String sdetails) {
+        this.sdetails = sdetails;
+    }
+
+    public int getTcount() {
+        return tcount;
+    }
+
+    public void setTcount(int tcount) {
+        this.tcount = tcount;
+    }
+}

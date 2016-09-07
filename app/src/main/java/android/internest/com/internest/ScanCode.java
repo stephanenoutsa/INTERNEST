98660271b9483473d54337d04fd7d8dc2fec0b59
Steph Noutsa <stephanenoutsa@yahoo.com>
2016-09-07 23:08:05 +0100

    Updates ScanCode activity to communicate with server

--- app/src/main/java/android/internest/com/internest/ScanCode.java
+++ app/src/main/java/android/internest/com/internest/ScanCode.java
@@ -1,5 +1,6 @@
 package android.internest.com.internest;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 //import android.support.design.widget.FloatingActionButton;
@@ -12,9 +13,18 @@ import android.widget.Toast;
 import com.google.zxing.integration.android.IntentIntegrator;
 import com.google.zxing.integration.android.IntentResult;
 
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 public class ScanCode extends AppCompatActivity {
 
     MyDBHandler dbHandler;
+    Scanned newScanned;
+    Trend newTrend;
+    Context context = this;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,13 +57,68 @@ public class ScanCode extends AppCompatActivity {
         if(scan != null) {
             final String contents = scan.getContents();
 
-            Trend trend = new Trend(contents);
-            dbHandler.addTrend(trend);
-
             if (contents != null) {
+                /**
+                 * Add trend item to server
+                 */
+                Trend trend = new Trend(contents);
+
+                // Trailing slash is needed
+                final String BASE_URL = "http://10.0.2.2:8080/internest/webapi/"; // Localhost value is 10.0.2.2
+
+                Retrofit retrofit = new Retrofit.Builder()
+                        .baseUrl(BASE_URL)
+                        .addConverterFactory(GsonConverterFactory.create())
+                        .build();
+
+                InternestService internestService = retrofit.create(InternestService.class);
+
+                Call<Trend> call = internestService.addTrend(trend);
+                call.enqueue(new Callback<Trend>() {
+                    @Override
+                    public void onResponse(Call<Trend> call, Response<Trend> response) {
+                        int statusCode = response.code();
+                        if (statusCode == 200) {
+                            newTrend = response.body();
+
+                            Toast.makeText(context, newTrend.getSdetails(), Toast.LENGTH_LONG).show();
+                        } else {
+                            Toast.makeText(context, "Couldn't update server", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+
+                    @Override
+                    public void onFailure(Call<Trend> call, Throwable t) {
+                        Toast.makeText(context, t.getLocalizedMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                });
+
                 if (URLUtil.isHttpsUrl(contents) || URLUtil.isHttpUrl(contents)) {
                     Scanned scanned = new Scanned(getString(R.string.scanned_type_url), contents);
+
+                    // Add scanned item to app database
                     dbHandler.addScanned(scanned);
+
+                    // Add scanned item to server database
+                    Call<Scanned> call1 = internestService.addScanned(scanned);
+                    call1.enqueue(new Callback<Scanned>() {
+                        @Override
+                        public void onResponse(Call<Scanned> call, Response<Scanned> response) {
+                            int statusCode = response.code();
+                            if (statusCode == 200) {
+                                newScanned = response.body();
+                                Toast.makeText(context, "Item added to remote database", Toast.LENGTH_SHORT).show();
+                            } else {
+                                Toast.makeText(context, "Couldn't update server", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+
+                        @Override
+                        public void onFailure(Call<Scanned> call, Throwable t) {
+                            Toast.makeText(context, t.getLocalizedMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    });
+
                     intent = new Intent(getApplicationContext(), URLDisplay.class);
                     intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
                     intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
@@ -63,7 +128,30 @@ public class ScanCode extends AppCompatActivity {
                     startActivity(intent);
                 } else {
                     Scanned scanned = new Scanned(getString(R.string.scanned_type_text), contents);
+
+                    // Add scanned item to app database
                     dbHandler.addScanned(scanned);
+
+                    // Add scanned item to server database
+                    Call<Scanned> call1 = internestService.addScanned(scanned);
+                    call1.enqueue(new Callback<Scanned>() {
+                        @Override
+                        public void onResponse(Call<Scanned> call, Response<Scanned> response) {
+                            int statusCode = response.code();
+                            if (statusCode == 200) {
+                                newScanned = response.body();
+                                Toast.makeText(context, "Item added to remote database", Toast.LENGTH_SHORT).show();
+                            } else {
+                                Toast.makeText(context, "Couldn't update server", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+
+                        @Override
+                        public void onFailure(Call<Scanned> call, Throwable t) {
+                            Toast.makeText(context, t.getLocalizedMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    });
+
                     intent = new Intent(this, TextDisplay.class);
                     intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
                     intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
@@ -78,8 +166,14 @@ public class ScanCode extends AppCompatActivity {
             }
         }
         else {
-            Toast.makeText(this, getResources().getString(R.string.empty_scan),
+            Toast.makeText(this, getString(R.string.empty_scan),
                     Toast.LENGTH_LONG).show();
         }
+    }
+
+    @Override
+    public void onBackPressed() {
+        Intent i = new Intent(this, Blog.class);
+        startActivity(i);
     }
 }

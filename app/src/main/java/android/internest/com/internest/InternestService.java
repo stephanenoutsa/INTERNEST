2983d1a144c98e83a1f276d7517d8f5c6aebfd39
Steph Noutsa <stephanenoutsa@yahoo.com>
2016-09-07 23:05:31 +0100

    Updates InternestService interface methods for Scanned and Trend items

--- app/src/main/java/android/internest/com/internest/InternestService.java
+++ app/src/main/java/android/internest/com/internest/InternestService.java
@@ -12,6 +12,7 @@ import retrofit2.http.Path;
  * Created by stephnoutsa on 8/31/16.
  */
 public interface InternestService {
+    // Start of methods for Posts
     @GET("posts")
     Call<List<Post>> getAllPosts();
 
@@ -20,4 +21,29 @@ public interface InternestService {
 
     @GET("posts/{postId}")
     Call<Post> getPost(@Path("postId") int postId);
+    // End of methods for Posts
+
+
+    // Start of methods for Scanned
+    @GET("scanned")
+    Call<List<Scanned>> getAllScanned();
+
+    @POST("scanned")
+    Call<Scanned> addScanned(@Body Scanned scanned);
+
+    @GET("scanned/{scannedId}")
+    Call<Scanned> getScanned(@Path("scannedId") int scannedId);
+    // End of methods for Scanned
+
+
+    // Start of methods for Trends
+    @GET("trends")
+    Call<List<Trend>> getAllTrends();
+
+    @POST("trends")
+    Call<Trend> addTrend(@Body Trend trend);
+
+    @GET("trends/{trendId}")
+    Call<Trend> getTrend(@Path("trendId") int trendId);
+    // End of methods for Trends
 }

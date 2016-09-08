54d8e2b29993a2d81f14ed1f7e17fd37bb35fd4c
Steph Noutsa <stephanenoutsa@yahoo.com>
2016-09-08 01:58:03 +0100

    Updates the User model to make it compatible with the server User model

--- app/src/main/java/android/internest/com/internest/User.java
+++ app/src/main/java/android/internest/com/internest/User.java
@@ -1,12 +1,25 @@
 package android.internest.com.internest;
 
 
+import com.google.gson.annotations.SerializedName;
 
 public class User {
 
     // Private variables
+    @SerializedName("id")
     int _uid;
-    String num, dob, gender;
+
+    @SerializedName("number")
+    String num;
+
+    @SerializedName("dob")
+    String dob;
+
+    @SerializedName("gender")
+    String gender;
+
+    @SerializedName("points")
+    int points;
 
     // Empty Constructor
     public User() {
@@ -14,18 +27,20 @@ public class User {
     }
 
     // Constructor
-    public User(int _uid, String num, String dob, String gender) {
+    public User(int _uid, String num, String dob, String gender, int points) {
         this._uid = _uid;
         this.num = num;
         this.dob = dob;
         this.gender = gender;
+        this.points = points;
     }
 
     // Constructor
-    public User(String num, String dob, String gender) {
+    public User(String num, String dob, String gender, int points) {
         this.num = num;
         this.dob = dob;
         this.gender = gender;
+        this.points = points;
     }
 
     // Getter and Setter methods
@@ -59,5 +74,13 @@ public class User {
 
     public void setGender(String gender) {
         this.gender = gender;
+    }
+
+    public int getPoints() {
+        return points;
+    }
+
+    public void setPoints(int points) {
+        this.points = points;
     }
 }

71ef47d9f0d50a1619fc084aebf8af2f6c33b093
Steph Noutsa <stephanenoutsa@yahoo.com>
2016-09-08 01:45:26 +0100

    Populates Trending activity with items from server database and no longer from app database

--- app/src/main/java/android/internest/com/internest/Trending.java
+++ app/src/main/java/android/internest/com/internest/Trending.java
@@ -12,13 +12,23 @@ import android.widget.AdapterView;
 import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 /**
  * Created by stephnoutsa on 6/1/16.
  */
 public class Trending extends Fragment {
+    List<Scanned> trends;
+    ListAdapter listAdapter;
+    ListView listView;
 
     public Trending() {
         // Required empty constructor
@@ -40,45 +50,71 @@ public class Trending extends Fragment {
         // Inflate the layout for this fragment
         final View rootView = inflater.inflate(R.layout.fragment_trending, container, false);
 
-        final MyDBHandler dbHandler = new MyDBHandler(getContext(), null, null, 1);
-
         final Handler handler = new Handler() {
             @Override
             public void handleMessage(Message msg) {
-                // Get all trending scanned items from database
-                List<Scanned> trends = dbHandler.getTrends();
+                /**
+                 * Get all trending items from server
+                 */
+                // Trailing slash is needed
+                final String BASE_URL = "http://10.0.2.2:8080/internest/webapi/"; // Localhost value is 10.0.2.2
 
-                // Get the scanned items into an adapter's list
-                ListAdapter listAdapter = new CustomAdapter(getContext(), trends);
+                Retrofit retrofit = new Retrofit.Builder()
+                        .baseUrl(BASE_URL)
+                        .addConverterFactory(GsonConverterFactory.create())
+                        .build();
 
-                // Set the adapter to display the scanned items
-                ListView listView = (ListView) rootView.findViewById(R.id.trends);
-                listView.setAdapter(listAdapter);
+                InternestService internestService = retrofit.create(InternestService.class);
 
-                listView.setOnItemClickListener(
-                        new AdapterView.OnItemClickListener() {
-                            @Override
-                            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                                TextView scanType = (TextView) view.findViewById(R.id.scanType);
-                                TextView scanDetails = (TextView) view.findViewById(R.id.scanDetails);
+                Call<List<Scanned>> call = internestService.getAllTrends();
+                call.enqueue(new retrofit2.Callback<List<Scanned>>() {
+                    @Override
+                    public void onResponse(Call<List<Scanned>> call, Response<List<Scanned>> response) {
+                        int statusCode = response.code();
+                        if (statusCode == 200) {
+                            trends = response.body();
 
-                                String sType = scanType.getText().toString();
-                                final String sDetails = scanDetails.getText().toString();
+                            // Get the trending items into an adapter's list
+                            listAdapter = new CustomAdapter(getContext(), trends);
 
-                                if (sType.equals(getString(R.string.scanned_type_text))) {
-                                    Intent intent = new Intent(getContext(), TextDisplay.class);
-                                    intent.putExtra("text", sDetails);
-                                    intent.putExtra("previous", "trending");
-                                    startActivity(intent);
-                                } else if (sType.equals(getString(R.string.url_simple))) {
-                                    Intent intent = new Intent(getContext(), URLDisplay.class);
-                                    intent.putExtra("url", sDetails);
-                                    intent.putExtra("previous", "trending");
-                                    startActivity(intent);
-                                }
-                            }
+                            // Set the adapter to display the scanned items
+                            listView = (ListView) rootView.findViewById(R.id.trends);
+                            listView.setAdapter(listAdapter);
+
+                            listView.setOnItemClickListener(
+                                    new AdapterView.OnItemClickListener() {
+                                        @Override
+                                        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                                            TextView scanType = (TextView) view.findViewById(R.id.scanType);
+                                            TextView scanDetails = (TextView) view.findViewById(R.id.scanDetails);
+
+                                            String sType = scanType.getText().toString();
+                                            final String sDetails = scanDetails.getText().toString();
+
+                                            if (sType.equals(getString(R.string.scanned_type_text))) {
+                                                Intent intent = new Intent(getContext(), TextDisplay.class);
+                                                intent.putExtra("text", sDetails);
+                                                intent.putExtra("previous", "trending");
+                                                startActivity(intent);
+                                            } else if (sType.equals(getString(R.string.url_simple))) {
+                                                Intent intent = new Intent(getContext(), URLDisplay.class);
+                                                intent.putExtra("url", sDetails);
+                                                intent.putExtra("previous", "trending");
+                                                startActivity(intent);
+                                            }
+                                        }
+                                    }
+                            );
+                        } else {
+                            Toast.makeText(getContext(), "Unable to communicate with server", Toast.LENGTH_SHORT).show();
                         }
-                );
+                    }
+
+                    @Override
+                    public void onFailure(Call<List<Scanned>> call, Throwable t) {
+                        Toast.makeText(getContext(), t.getLocalizedMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                });
             }
         };
 

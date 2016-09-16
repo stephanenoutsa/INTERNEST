9a4c6f4d1a26785f4a06b6a846f414345765b2ec
Steph Noutsa <stephanenoutsa@yahoo.com>
2016-09-16 14:24:50 +0100

    Pressing back from CaptureActivityPortrait starts Blog.class

--- app/src/main/java/android/internest/com/internest/CaptureActivityPortrait.java
+++ app/src/main/java/android/internest/com/internest/CaptureActivityPortrait.java
@@ -2,8 +2,15 @@ package android.internest.com.internest;
 
 
 
+import android.content.Intent;
+
 import com.journeyapps.barcodescanner.CaptureActivity;
 
 public class CaptureActivityPortrait extends CaptureActivity {
-
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        Intent i = new Intent(this, Blog.class);
+        startActivity(i);
+    }
 }

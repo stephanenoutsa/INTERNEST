bbeac785da32705f083d25c38987dc4b5ee22451
Steph Noutsa <stephanenoutsa@yahoo.com>
2016-09-19 22:03:31 +0100

    Adds icons for the ContactUs activity

--- app/build/generated/source/r/debug/android/support/v7/appcompat/R.java
+++ app/build/generated/source/r/debug/android/support/v7/appcompat/R.java
@@ -465,10 +465,10 @@ public final class R {
 		public static final int abc_textfield_search_activated_mtrl_alpha = 0x7f020048;
 		public static final int abc_textfield_search_default_mtrl_alpha = 0x7f020049;
 		public static final int abc_textfield_search_material = 0x7f02004a;
-		public static final int notification_template_icon_bg = 0x7f020059;
+		public static final int notification_template_icon_bg = 0x7f02005c;
 	}
 	public static final class id {
-		public static final int action0 = 0x7f0e00a4;
+		public static final int action0 = 0x7f0e00ab;
 		public static final int action_bar = 0x7f0e0064;
 		public static final int action_bar_activity_content = 0x7f0e0000;
 		public static final int action_bar_container = 0x7f0e0063;
@@ -477,7 +477,7 @@ public final class R {
 		public static final int action_bar_subtitle = 0x7f0e0045;
 		public static final int action_bar_title = 0x7f0e0044;
 		public static final int action_context_bar = 0x7f0e0065;
-		public static final int action_divider = 0x7f0e00a8;
+		public static final int action_divider = 0x7f0e00af;
 		public static final int action_menu_divider = 0x7f0e0002;
 		public static final int action_menu_presenter = 0x7f0e0003;
 		public static final int action_mode_bar = 0x7f0e0061;
@@ -488,9 +488,9 @@ public final class R {
 		public static final int always = 0x7f0e003a;
 		public static final int beginning = 0x7f0e0038;
 		public static final int buttonPanel = 0x7f0e004e;
-		public static final int cancel_action = 0x7f0e00a5;
+		public static final int cancel_action = 0x7f0e00ac;
 		public static final int checkbox = 0x7f0e005c;
-		public static final int chronometer = 0x7f0e00ab;
+		public static final int chronometer = 0x7f0e00b2;
 		public static final int collapseActionView = 0x7f0e003b;
 		public static final int contentPanel = 0x7f0e0054;
 		public static final int custom = 0x7f0e005a;
@@ -500,7 +500,7 @@ public final class R {
 		public static final int disableHome = 0x7f0e0015;
 		public static final int edit_query = 0x7f0e0066;
 		public static final int end = 0x7f0e0028;
-		public static final int end_padder = 0x7f0e00b0;
+		public static final int end_padder = 0x7f0e00b7;
 		public static final int expand_activities_button = 0x7f0e0048;
 		public static final int expanded_menu = 0x7f0e005b;
 		public static final int home = 0x7f0e0004;
@@ -508,12 +508,12 @@ public final class R {
 		public static final int icon = 0x7f0e004c;
 		public static final int ifRoom = 0x7f0e003c;
 		public static final int image = 0x7f0e0049;
-		public static final int info = 0x7f0e00af;
-		public static final int line1 = 0x7f0e00a9;
-		public static final int line3 = 0x7f0e00ad;
+		public static final int info = 0x7f0e00b6;
+		public static final int line1 = 0x7f0e00b0;
+		public static final int line3 = 0x7f0e00b4;
 		public static final int listMode = 0x7f0e0012;
 		public static final int list_item = 0x7f0e004b;
-		public static final int media_actions = 0x7f0e00a7;
+		public static final int media_actions = 0x7f0e00ae;
 		public static final int middle = 0x7f0e0039;
 		public static final int multiply = 0x7f0e002e;
 		public static final int never = 0x7f0e003d;
@@ -547,13 +547,13 @@ public final class R {
 		public static final int src_atop = 0x7f0e0030;
 		public static final int src_in = 0x7f0e0031;
 		public static final int src_over = 0x7f0e0032;
-		public static final int status_bar_latest_event_content = 0x7f0e00a6;
+		public static final int status_bar_latest_event_content = 0x7f0e00ad;
 		public static final int submit_area = 0x7f0e006f;
 		public static final int tabMode = 0x7f0e0014;
-		public static final int text = 0x7f0e00ae;
-		public static final int text2 = 0x7f0e00ac;
+		public static final int text = 0x7f0e00b5;
+		public static final int text2 = 0x7f0e00b3;
 		public static final int textSpacerNoButtons = 0x7f0e0057;
-		public static final int time = 0x7f0e00aa;
+		public static final int time = 0x7f0e00b1;
 		public static final int title = 0x7f0e004d;
 		public static final int title_template = 0x7f0e0052;
 		public static final int topPanel = 0x7f0e0051;
